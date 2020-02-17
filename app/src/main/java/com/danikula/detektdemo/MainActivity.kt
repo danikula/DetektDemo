bb16d441cae209c8bb50cebcd67fd9d9ccc4bf6e
Alexey Danilov <a_danilov@flo.health>
2020-02-17 17:39:26 +0300

    Reproducing ForbiddenMethodCall issue

--- app/src/main/java/com/danikula/detektdemo/MainActivity.kt
+++ app/src/main/java/com/danikula/detektdemo/MainActivity.kt
@@ -1,12 +1,17 @@
 package com.danikula.detektdemo
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import java.lang.System.gc
 
-class MainActivity :AppCompatActivity(){
+class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        java.lang.System.gc()
+        System.gc()
+        gc()
     }
 }
